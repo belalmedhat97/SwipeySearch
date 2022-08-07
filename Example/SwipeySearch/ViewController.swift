57744e3668410ac41fe0fc6f4205ce2dbdcb9db1
Belal <belal1997medhat@gmail.com>
2022-08-07 02:10:59 +0200

    added the base SwipeySearchview

--- Example/SwipeySearch/ViewController.swift
+++ Example/SwipeySearch/ViewController.swift
@@ -7,16 +7,18 @@
 //
 
 import UIKit
-
+import SwipeySearch
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var searchBar: SwipeySearchClass!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        searchBar.searchButton.backgroundColor = .red
         // Dispose of any resources that can be recreated.
     }
 
---
+++ SwipeySearch/Classes/SwipeySearchClass.swift
@@ -0,0 +1,73 @@
+//
+//  SwipeySearchClass.swift
+//  SwipeySearch
+//
+//  Created by belal medhat on 03/08/2022.
+//
+
+import UIKit
+
+@IBDesignable public class SwipeySearchClass: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak public var searchView: UIView!
+    
+    @IBOutlet weak public var searchButton: UIButton!
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+       didSet {
+           contentView.layer.cornerRadius = cornerRadius
+           contentView.layer.masksToBounds = cornerRadius > 0
+       }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+       didSet {
+           contentView.layer.borderWidth = borderWidth
+       }
+    }
+    @IBInspectable var borderColor: UIColor? {
+       didSet {
+           contentView.layer.borderColor = borderColor?.cgColor
+       }
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+   public override init(frame: CGRect) {
+        super.init(frame: frame)
+       initMe()
+
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initMe()
+
+    }
+    public override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        initMe()
+
+    }
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        initMe()
+    }
+
+    private func initMe(){
+        
+        let bundle = Bundle(for: SwipeySearchClass.self)
+        let view = UINib(nibName: "SwipeySearchView", bundle: bundle).instantiate(withOwner: self, options: nil).first as! UIView
+        view.frame = self.bounds
+        view.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        addSubview(view)
+
+    
+    }
+}
+
